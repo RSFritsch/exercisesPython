30a5e7efa862ef462f7a470822fe4833036dbaf9
RSFritsch <107947476+RSFritsch@users.noreply.github.com>
2022-09-08 23:09:26 -0300

    Add files via upload
    exercício de chamada de funções

---
+++ lyrics_runaway.py
@@ -0,0 +1,93 @@
+def print_lyrics_1():
+    print("I was listenin' to the ocean")
+    print("I saw a face in the sand")
+    print("But when I picked it up")
+    print("Then it vanished away from my hands, down")
+    print("                                     ")
+
+def print_lyrics_2():
+    print("I had a dream, I was seven")
+    print("Climbin' my way in a tree")
+    print("I saw a piece of Heaven")
+    print("Waitin' in patience for me, down")
+    print("                                     ")
+
+def print_lyrics_3():
+    print("And I was runnin' far away") 
+    print("Would I run off the world someday?")
+    print("Nobody knows, nobody knows")
+    print("And I was dancing in the rain")
+    print("I felt alive and I can't complain")
+    print("                                     ")
+
+def print_lyrics_4():
+    print("But no, take me home")
+    print("Take me home where I belong")
+    print("I can't take it anymore")
+    print("                                     ")
+
+def print_lyrics_5():
+    print("I was painting a picture")
+    print("The picture was a painting of you")
+    print("And for a moment I thought you were here")
+    print("But then again, it wasn't true, down")
+    print("                                     ")
+
+def print_lyrics_6():
+    print("And all this time I have been lyin'")
+    print("Oh, lyin' in secret to myself")
+    print("I've been putting sorrow on the farthest place on my shelf")
+    print("La-di-da")
+    print("                                                  ")
+
+def print_lyrics_7():
+    print("But no, take me home")
+    print("Take me home where I belong")
+    print("I got no other place to go")
+    print("No, take me home")
+    print("Take me home where I belong")
+    print("I got no other place to go")
+
+def print_lyrics_81():    
+    print("No, take me home")
+    print("Take me home where I belong")
+    print("I can't take it anymore")
+    print("                             ")
+
+def print_lyrics_8():
+    print("But I kept runnin' for a soft place to fall")
+    print("And I kept runnin' for a soft place to fall")
+    print("And I kept runnin' for a soft place to fall")
+    print("And I kept runnin' for a soft place to fall")
+    print("                                       ")
+
+def print_lyrics_9():
+    print("And I was runnin' far away")
+    print("Would I run off the world someday?")
+    print("                                      ")
+
+def print_lyrics_10():
+    print("                              ")
+    print("No, take me home, home where I belong, no, no")
+    print("No, take me home, home where I belong, oh, oh, oh")
+    print("No, take me home, home where I belong, no, no")
+    print("No, take me home, home where I belong")
+    print("I can't take it anymore")
+    
+
+def repeat_lyrics():
+    print_lyrics_1()
+    print_lyrics_2()
+    print_lyrics_3()
+    print_lyrics_4()
+    print_lyrics_5()
+    print_lyrics_6()
+    print_lyrics_3()
+    print_lyrics_7()
+    print_lyrics_81()
+    print_lyrics_8()
+    print_lyrics_9()
+    print_lyrics_7()
+    print_lyrics_10()
+
+repeat_lyrics()
