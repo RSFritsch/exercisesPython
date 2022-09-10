26d0a868f3833ddf5b5bf532b8edfba65eb08448
RSFritsch <107947476+RSFritsch@users.noreply.github.com>
2022-09-09 21:10:56 -0300

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,20 @@
+def print_linha1():
+    print("+ - - - - + - - - - +")
+
+def print_linha2():
+    print("|         |         |")
+
+def print_grade():
+    print_linha1()
+    print_linha2()
+    print_linha2()
+    print_linha2()
+    print_linha2()
+    print_linha1()
+    print_linha2()
+    print_linha2()
+    print_linha2()
+    print_linha2()
+    print_linha1()
+
+print_grade()
